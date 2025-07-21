ef8b627a03171ea3e38ee3e3bce3cb2ac9e66237
Al-Amin <alaminkhanshakil@gmail.com>
2025-07-21 14:27:26 +0600

    Add conpany name and logo

--- src/assets/index.js
+++ src/assets/index.js
@@ -21,10 +21,9 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
+import microverse from "./company/microverse.png";
+import eleganttex from "./company/eleganttex.png";
+import tttr from "./company/tttr.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -52,10 +51,9 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
+  microverse,
+  eleganttex,
+  tttr,
   carrent,
   jobit,
   tripguide,
